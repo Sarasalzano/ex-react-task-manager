65aa1cd305f382dee0bf45bada55817287bd3214
Sara Salzano <sarasalzano95@gmail.com>
2026-01-07 09:41:22 +0100

    aggiungi stile inline alla row

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -1,21 +1,22 @@
+import React from "react";
 
- function TaskRow({ title, status, createdAt }) {
-    let colore
-    if (status === "To do") {
-        colore = "red"
-    } else if (status ==="Doing") {
-        colore = "yellow"
-    } else if (status === "Done") {
-        colore = "green"
-    }
+function TaskRow({ title, status, createdAt }) {
+  let colore;
+  if (status === "To do") {
+    colore = "red";
+  } else if (status === "Doing") {
+    colore = "yellow";
+  } else if (status === "Done") {
+    colore = "green";
+  }
 
   return (
     <tr>
       <td>{title}</td>
-      <td style={{backgroundColor: colore}}>{status}</td>
+      <td style={{ backgroundColor: colore }}>{status}</td>
       <td>{createdAt}</td>
     </tr>
   );
 }
 
-export default React.memo(TaskRow)
+export default React.memo(TaskRow);
